2a760f01eddf249ffa70573125b00ea2f0b1298a
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-12-05 18:32:11 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         HospitalDatabase hospitalDatabase = new HospitalDatabase();
         Department department1 = new Department("Кардиологическое", 0);
         Department department2 = new Department("Нейрохирургическое", 0);
@@ -15,17 +15,44 @@ public class Main {
         hospitalDatabase.addPatient(patient3);
         hospitalDatabase.addPatientToDepartment(patient1, department1);
         hospitalDatabase.addPatientToDepartment(patient2, department1);
-        hospitalDatabase.addPatientToDepartment(patient3, department2);
-        hospitalDatabase.printPatientsInfo();
+        hospitalDatabase.addPatientToDepartment(patient3, department1);
+        /*hospitalDatabase.printPatientsInfo();
         hospitalDatabase.printDepartmentsInfo();
-        String url = "jdbc:postgresql://localhost:5432/test_db";
-        String user = "postgres";
-        String password = "416256";
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            ConnectionUtil.displayPatients(conn);
-            ConnectionUtil.displayDepartments(conn);
-        } catch (SQLException e) {
-            e.printStackTrace();
+         */
+        ConnectionUtil db = new ConnectionUtil();
+        Connection conn = db.connect_to_db("test_db", "postgres", "416256");
+        // добавление пациента
+        /*PreparedStatement insertStmt = conn.prepareStatement("INSERT INTO Patients(full_name, age, gender) VALUES(?,?,?)");
+        insertStmt.setString(1,patient2.getFullName());
+        insertStmt.setInt(2,patient2.getAge());
+        insertStmt.setString(3,patient2.getGender());
+        insertStmt.executeUpdate();
+         */
+
+
+        // добавление отделения
+        /*PreparedStatement insertStmt1 = conn.prepareStatement("INSERT INTO Departments(full_name, patients) VALUES(?,?)");
+        insertStmt1.setString(1,department1.getName());
+        insertStmt1.setInt(2,department1.getPatientCount());
+        insertStmt1.executeUpdate();
+         */
+
+        //удаление
+        /*PreparedStatement deletStmt = conn.prepareStatement("DELETE FROM patients WHERE full_name = ?");
+        deletStmt.setString(1, "Жора ЫЫЫ");
+        deletStmt.executeUpdate();
+         */
+        //вывод данных в терминал
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM patients");
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            System.out.printf("full_name:%s age:%s gender:%s%n", rs.getString("full_name"), rs.getInt("age"), rs.getString("gender"));
+        }
+        PreparedStatement stmt1 = conn.prepareStatement("SELECT * FROM departments");
+        System.out.println();
+        ResultSet rs1 = stmt1.executeQuery();
+        while (rs1.next()) {
+            System.out.printf("full_name:%s patients:%s%n", rs1.getString("full_name"), rs1.getInt("patients"));
         }
     }
 }

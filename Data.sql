56e6cf178048282573691304faa8fc564847f4b5
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-12-05 18:33:23 +0300

    Data.sql

---
+++ Data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Patients (
+    full_name VARCHAR(50),
+    age numeric(50),
+    Gender varchar(50)
+),
+CREATE TABLE Departments (
+    full_name VARCHAR(50),
+    patients numeric(50)
+)

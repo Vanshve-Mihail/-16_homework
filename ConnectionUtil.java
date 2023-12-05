55c1b6db5ac1e75c1fdb7ede403d946ebc1dbce3
Vanshev Mihail <112972907+Vanshve-Mihail@users.noreply.github.com>
2023-12-05 18:32:43 +0300

    ConnectionUtil.java

--- ConnectionUtil.java
+++ ConnectionUtil.java
@@ -1,53 +1,20 @@
 import java.sql.*;
 public class ConnectionUtil {
-    public static void displayPatients(Connection conn) {
+    public Connection connect_to_db(String dbname, String user, String pass) {
+
+        Connection conn = null;
         try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Patients");
-
-            ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(metaData.getColumnName(i) + "\t");
-            }
-            System.out.println();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pass);
+            if (conn != null) {
+                System.out.println("Connection Established");
+            } else {
+                System.out.println("Connection Failed");
             }
 
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-    }
-    public static void displayDepartments(Connection conn) {
-        try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM Departments");
-
-            ResultSetMetaData metaData = rs.getMetaData();
-            int columnCount = metaData.getColumnCount();
-
-            for (int i = 1; i <= columnCount; i++) {
-                System.out.print(metaData.getColumnName(i) + "\t");
-            }
-            System.out.println();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        return conn;
     }
 }
